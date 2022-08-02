da487e7691285c36090ec13af0e795fd14f13257
peteto1213 <peteto1213@gmail.com>
2022-08-02 12:51:37 +0100

    started design with scss strcture

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,9 +1,19 @@
 import React from 'react';
+import { BrowserRouter as Router, Routes, Route, Navigate } from 'react-router-dom'
 
 function App() {
   return (
     <>
-      <h1>Hello World</h1>
+      <Router>
+        <div className="container">
+          <Routes>
+            {/* <Route paht='/' element={<Home/>} /> */}
+
+
+            <Route paht='*' element={<Navigate to="/" replace />} />
+          </Routes>
+        </div>
+      </Router>
     </>
   );
 }
